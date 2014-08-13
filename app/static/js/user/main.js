f8b57bdeb1e4f4de83056c7ef21b3b278d447924
Logan Richardson <g.logan.richardson@gmail.com>
2014-08-12 23:25:33 -0500

    iron-chef class exercise

--- app/static/js/user/main.js
+++ app/static/js/user/main.js
@@ -2,7 +2,32 @@
   'use strict';
 
   $(document).ready(function(){
+    $('#hide').click(hide);
+    $('#show').click(show);
+    $('form').submit(addRecipe);
   });
+
+  function addRecipe(e){  //stops the form from posting itself to a new url//
+    var data = $('form').serialize(), // gets the data and puts it in a row. Data//
+        type = $('form').attr('method'), // gets an attribute out of a tag. Verb//
+        url = $('form').attr('action'); // gets the link. URL//
+    //clearing form//
+    $('input, textarea').val('');
+    //hiding form//
+    hide();
+    $.ajax({url:url, type:type, data:data, dataType:'html', success:function(html){
+      $('#recipes').prepend(html); //adds the new recipe to the front of the list of recipes. Append would put it at the end//
+    }}); //sends the data over the internet using ajax. Waits for a response. Plugs in the variables declared above and dataType plus success function//
+    e.preventDefault(); //stops the page from loading a new page (default)//
+  }
+
+  function hide(){
+    $('form').fadeOut();
+  }
+
+  function show(){
+    $('form').fadeIn();
+  }
 
 })();
 
